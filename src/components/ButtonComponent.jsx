e5ae89b04b02bc6dd15a1c4a6eca838652f524fa
Lucien Ramassamy <lucien.ramassamy@hetic.net>
2021-07-22 15:26:26 +0200

    Feat(ButtonComponent): add default text

--- src/components/ButtonComponent.jsx
+++ src/components/ButtonComponent.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 // STYLE
 import '../assets/styles/button.scss'
 
-const ButtonComponent = () => {
+const ButtonComponent = (props) => {
   return (
     <div className="button--component">
-      <button>default text</button>
+      <button>{props.text ? props.text : "default text"}</button>
     </div>
   );
 }
