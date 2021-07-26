ac89ef49bc0760fd9fa5e221c6259c776cc04940
Lucien Ramassamy <lucien.ramassamy@hetic.net>
2021-07-26 14:52:11 +0200

    feat(Firestore): starts firestore connexion

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,13 +2,38 @@ import React from 'react';
 import '../assets/styles/login.scss'
 import { InputComponent } from '../components'
 import { useHistory } from 'react-router-dom'
-  
+import { firebase } from '../firebase'
 const Login = () => {
 
   const history = useHistory();
 
   const login = () => {
-    history.push('/accueil');
+
+    firebase
+    .auth()
+    .signInWithEmailAndPassword('melvin@gmail.com', '123456')
+    .then( response  => {
+        const uid = response.user.uid
+        const usersRef = firebase.firestore().collection('users')
+        usersRef
+        .doc(uid)
+        .get()
+        .then(firestoreDocument => {
+            if (!firestoreDocument.exists) {
+            alert("User does not exist anymore.")
+            console.error(firestoreDocument)
+            return;
+            }
+            const user = firestoreDocument.data()
+            console.log(user)
+            history.push('/accueil');
+            })
+        .catch(error => { console.error(error) });
+      })
+    .catch(error => {
+      console.error(error)
+    })
+
   }
 
   return (
