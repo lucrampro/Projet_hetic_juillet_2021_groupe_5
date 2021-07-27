85f305154b3fa0604bc5230d318753f0435e410e
Melvin Debot <melvin@exceenis.com>
2021-07-27 12:20:10 +0200

    STYLE(Notifications): modification style component

--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 // STYLES
-import '../assets/styles/notifications.scss'
+import '../assets/styles/notification.scss';
+
+const getArrayNotif = ["bin", "mailbox"];
 
 const Notifications = () => (
   <>
-    <p>Notifications</p>
+    <div className="notification">
+      <div className="notifications-title">
+        {/* <img src='/imgs/icons/arrow-back.png' alt="" srcSet="" /> */}
+        <h3>Notifications</h3>
+      </div>
+      {
+        getArrayNotif.map(function (elem) {
+          return <div className="notifications-bloc">
+            <h4>Boite aux lettre</h4>
+            <h4>12:40</h4>
+          </div>
+        })
+
+      }
+      
+    </div>
   </>
 );
 
