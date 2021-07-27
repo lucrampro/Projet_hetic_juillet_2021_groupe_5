47691fbcd57a334033223ba945149991cdf083ce
Melvin Debot <melvin@exceenis.com>
2021-07-27 15:15:20 +0200

    STYLE(EntranceHall): modification style component

--- src/components/EntranceHallDetails.jsx
+++ src/components/EntranceHallDetails.jsx
@@ -4,11 +4,15 @@ import '../assets/styles/entranceHallDetails.scss'
 
 const EntranceHallDetails = () => (
   <>
-    <button className="peripherique--menu" style={{ margin: "5%"}} >
+    <button className="peripherique--menu" style={{ margin: "5%" }} >
+      <img src='/imgs/icons/icon-bin.png' alt="" srcSet="" className="peripherique--img" />
       <p>Hall</p>
+      <img src='/imgs/icons/arrow-back.png' alt="" srcSet="" className="img--arrow" />
     </button>
     <button className="peripherique--menu">
+      <img src='/imgs/icons/Stair.png' alt="" srcSet="" className="peripherique--img" />
       <p>Escalier</p>
+      <img src='/imgs/icons/arrow-back.png' alt="" srcSet="" className="img--arrow" />
     </button>
   </>
 );
