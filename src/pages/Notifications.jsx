5ea17fd741e992b332622fa30fb06ab766998a7b
Melvin Debot <melvin@exceenis.com>
2021-07-27 11:39:15 +0200

    FEAT(Notification): create component

---
+++ src/pages/Notifications.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// STYLES
+import '../assets/styles/notifications.scss'
+
+const Notifications = () => (
+  <>
+    <p>Notifications</p>
+  </>
+);
+
+export default Notifications;
